5b8d4b65c9d0df2962a7013fa37e1feb5965a4c9
Amine Tahiri Alaoui <atahiri-@e2r6p5.1337.ma>
2025-10-15 15:06:05 +0100

    Added: some helper utils and the main header file

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 11:11:20 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/10/15 15:05:35 by atahiri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE (32)
+# endif
+
+char			*get_next_line(int fd);
+void			*ft_realloc(void *mem, unsigned long size);
+unsigned long	ft_strlen(const char *str);
+void			ft_strnextend(char **s1, const char *s2, unsigned long n);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 14:27:28 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/10/15 15:04:38 by atahiri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_realloc(void *mem, unsigned long size)
+{
+	unsigned char	*new;
+	unsigned int	i;
+
+	new = malloc(size);
+	if (new == NULL)
+		return (NULL);
+	if (mem != NULL)
+	{
+		i = 0;
+		while (i < size)
+		{
+			new[i] = ((unsigned char *)mem)[i];
+			i++;
+		}
+		free(mem);
+	}
+	return (new);
+}
+
+unsigned long	ft_strlen(const char *str)
+{
+	unsigned long	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+void	ft_strnextend(char **s1, const char *s2, unsigned long n)
+{
+	unsigned long	s1_len;
+	unsigned long	s2_len;
+	unsigned long	total_len;
+	unsigned long	i;
+
+	if (s1 == NULL || s2 == NULL)
+		return ;
+	if (s1 == NULL)
+		s1_len = 0;
+	else
+		s1_len = ft_strlen(*s1);
+	s2_len = ft_strlen(s2);
+	if (n < s2_len)
+		s2_len = n;
+	total_len = s1_len + s2_len + 1;
+	*s1 = ft_realloc(*s1, total_len);
+	i = 0;
+	while (i < s2_len)
+	{
+		(*s1)[s1_len + i] = s2[i];
+		i++;
+	}
+	(*s1)[s1_len + i] = '\0';
+}
